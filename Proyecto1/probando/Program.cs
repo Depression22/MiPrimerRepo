a359a713a4b653533b87f676cfaa143d36a4fe94
JaredQ <jaredqc.22@hotmail.com>
2025-01-01 18:45:21 -0600

    cambios nuevos

--- Proyecto1/probando/Program.cs
+++ Proyecto1/probando/Program.cs
@@ -1,4 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
-string = stripe_api_key = "sk_test_4esd7866sghjszal9821";
+string = stripe_api_key = "sk_test_4esd7866sghjszal985656g21";
